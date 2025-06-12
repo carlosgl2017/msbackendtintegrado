d548d5e933171c04b6c00e17f39aa51888ba0c1a
Juan Carlos Garcia Lima <jgarcia@adm.sanmartin.com.bo>
2025-06-12 17:26:58 -0400

    subida

--- kubernetes/msvc-usuarios/src/main/java/com/jgarcia/springcloud/usuarios/controllers/UsuarioController.java
+++ kubernetes/msvc-usuarios/src/main/java/com/jgarcia/springcloud/usuarios/controllers/UsuarioController.java
@@ -1,8 +1,30 @@
 package com.jgarcia.springcloud.usuarios.controllers;
 
+import com.jgarcia.springcloud.usuarios.models.entity.Usuario;
+import com.jgarcia.springcloud.usuarios.services.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class UsuarioController {
-
+    @Autowired
+    private UsuarioService usuarioService;
+    @GetMapping("/")
+    public List<Usuario> listar() {
+        return usuarioService.findAll();
+    }
+    @GetMapping("/id")
+    private ResponseEntity<Usuario> usuario(@PathVariable Long id) {
+        Optional<Usuario> usuarioOptional = usuarioService.findById(id);
+        if(usuarioOptional.isPresent()) {
+            return usuarioOptional.get();
+        }
+        return null;
+    }
 }
--- kubernetes/msvc-usuarios/src/main/java/com/jgarcia/springcloud/usuarios/services/UsuarioServiceImpl.java
+++ kubernetes/msvc-usuarios/src/main/java/com/jgarcia/springcloud/usuarios/services/UsuarioServiceImpl.java
@@ -1,29 +1,39 @@
 package com.jgarcia.springcloud.usuarios.services;
 
 import com.jgarcia.springcloud.usuarios.models.entity.Usuario;
+import com.jgarcia.springcloud.usuarios.repositories.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
+
 @Service
-public class UsuarioServiceImpl implements  UsuarioService{
+public class UsuarioServiceImpl implements UsuarioService {
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
     @Override
     public List<Usuario> findAll() {
-        return List.of();
+        return (List<Usuario>) usuarioRepository.findAll();
     }
 
     @Override
+    @Transactional(readOnly = true)
     public Optional<Usuario> findById(Long id) {
-        return Optional.empty();
+        return usuarioRepository.findById(id);
     }
 
     @Override
+    @Transactional
     public Usuario save(Usuario usuario) {
-        return null;
+        return usuarioRepository.save(usuario);
     }
 
     @Override
+    @Transactional
     public void deleteById(Long id) {
-
+        usuarioRepository.deleteById(id);
     }
 }
